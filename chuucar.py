3e125a1a6eb63e1debb719887893c37ad95c0570
ChaeheeKang-GitHub <ubuntu@ip-172-31-38-148.ap-northeast-2.compute.internal>
2022-11-10 16:09:17 +0000

    날짜 한국시간으로 변경

--- chuucar.py
+++ chuucar.py
@@ -1,6 +1,6 @@
 import json
 import slack_sdk
-from datetime import date
+from datetime import date, datetime, timezone, timedelta
 
 with open('../GitOps-SlackBot/token.json', 'r') as token_json:
     secret_token = json.load(token_json)
@@ -8,25 +8,21 @@ with open('../GitOps-SlackBot/token.json', 'r') as token_json:
 SLACK_TOKEN = secret_token["token"]
 SLACK_CHANNEL = "#ch-slackbot-test"
 
-b_day='{"ch": "20000817","yg": "20000207","jw": "19990311","test": "20001110","test2":"11991110"}'
+b_day='{"ch": "20000817","yg": "20000207","jw": "19990311","test": "20001110","test2":"119911111"}'
 b_dict = json.loads(b_day)
 
 def chuucar_send_msg(slack_msg):
     client = slack_sdk.WebClient(token=SLACK_TOKEN)
     client.chat_postMessage(channel=SLACK_CHANNEL,text=slack_msg)
-    
-today = date.today()
 
-for key,val in b_dict.items():
-    #date_of_birth = date(2022,11,10)    
+#datetime 한국시간 변경
+KST = timezone(timedelta(hours=9))
+today=datetime.now(KST)
+
+for key,val in b_dict.items():   
     date_of_birth = date(int(val[0:4]), int(val[4:6]), int(val[6:8]))
-    #print(key,date_of_birth)
 
-    birthday = date(today.year, date_of_birth.month, date_of_birth.day)
-    days_until_birthday = (birthday-today).days
-    days_alive = (today-date_of_birth).days
-
-    if days_until_birthday == 0:
+    if today.month==date_of_birth.month and today.day == date_of_birth.day:
         chat = "Today is "+ key + "'s Birthday."+" Let's Celebrate Together!!"
         chuucar_send_msg(chat)
 
